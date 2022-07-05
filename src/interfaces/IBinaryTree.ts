73d7382640aa9450da37ef7349737c543d213569
MJoon-Jung <gjgjajaj31@gmail.com>
2022-07-05 14:13:28 +0900

    add interface binary search tree - implementation binary search tree

---
+++ src/interfaces/IBinaryTree.ts
@@ -0,0 +1,10 @@
+import { Node } from '@/src/tree/Node';
+
+export interface IBinarySearchTree {
+  insert(node: Node): void; // 노드 삽입
+  search(value: number): Node; // 트리에서 값 탐색
+  delete(): void;
+  inOrderTraversal(): Node[];
+  preOrderTraversal(): Node[];
+  levelOrderTraversal(): Node[];
+}
---
+++ src/tree/BinarySearchTree.ts
@@ -0,0 +1,4 @@
+import { IBinarySearchTree } from '@/src/interfaces';
+import { Node } from '@/src/tree/Node';
+
+export class BinarySearchTree implements IBinarySearchTree {}
